781b70d26ea4eb5be3f03d5dd59b5d4a938bfc6e
Felipe Santos <felipefss@gmail.com>
2024-03-30 22:13:02 +0000

    Initial setup
    - main and login pages

---
+++ src/app/login/page.tsx
@@ -0,0 +1,15 @@
+export default async function LoginPage() {
+  return (
+    <>
+      <header>
+        <h1>
+          <span>Welcome to</span>
+          <span>FelpsCal</span>
+        </h1>
+      </header>
+      <main>
+        <button type="button">Login with Google</button>
+      </main>
+    </>
+  );
+}
