282cc23c263707b0882e47ce32d2243d57feff25
Felipe Santos <felipefss@gmail.com>
2024-04-08 22:36:29 +0100

    feat: finish styling login page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,10 @@
 import "@/styles/globals.css";
+import "@radix-ui/themes/styles.css";
 
 import { Inter } from "next/font/google";
 
 import { TRPCReactProvider } from "@/trpc/react";
+import { Theme } from "@radix-ui/themes";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -21,7 +23,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`font-sans ${inter.variable}`}>
-        <TRPCReactProvider>{children}</TRPCReactProvider>
+        <TRPCReactProvider>
+          <Theme accentColor="indigo" grayColor="auto" radius="medium">
+            {children}
+          </Theme>
+        </TRPCReactProvider>
       </body>
     </html>
   );
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,14 +1,32 @@
+import { Button } from "@radix-ui/themes";
+import { Concert_One } from "next/font/google";
+import { GoogleLogo } from "@phosphor-icons/react/dist/ssr";
+
+const rubik = Concert_One({
+  subsets: ["latin"],
+  weight: "400",
+});
+
 export default async function LoginPage() {
   return (
     <>
-      <header>
-        <h1>
-          <span>Welcome to</span>
-          <span>FelpsCal</span>
+      <header className="mx-auto w-1/3 pt-16 text-center">
+        <h1 className="flex flex-col gap-6">
+          <span className="self-start text-7xl font-semibold">Welcome to</span>
+          <span
+            className={`font-sans ${rubik.className} text-7xl text-blue-500`}
+          >
+            FelpsCal
+          </span>
         </h1>
       </header>
-      <main>
-        <button type="button">Login with Google</button>
+      <main className="mt-12 flex items-center justify-center">
+        <div className="border border-solid border-slate-500 px-56 py-20">
+          <Button className="hover:cursor-pointer" type="button">
+            <GoogleLogo size={24} />
+            <span className="text-lg font-semibold">Login with Google</span>
+          </Button>
+        </div>
       </main>
     </>
   );
