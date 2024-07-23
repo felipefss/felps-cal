d00fad6fd4490db35c76caa04dc6d075cf27a5d2
Felipe Santos <felipefss@gmail.com>
2024-07-23 16:24:34 +0100

    feat: create nav items

---
+++ src/app/_components/link.tsx
@@ -0,0 +1,22 @@
+import { ReactNode } from 'react';
+import { Button } from '@radix-ui/themes';
+import NextLink from 'next/link';
+
+interface Props {
+  href: string;
+  children: ReactNode;
+}
+
+export function Link({ href, children }: Props) {
+  return (
+    <Button
+      variant="ghost"
+      asChild={true}
+      size="3"
+      color="sky"
+      style={{ width: '100%' }}
+    >
+      <NextLink href={href}>{children}</NextLink>
+    </Button>
+  );
+}
--- src/app/_components/nav-bar.tsx
+++ src/app/_components/nav-bar.tsx
@@ -1,8 +1,9 @@
 import { Avatar, Separator, Text } from '@radix-ui/themes';
+import { Link } from './link';
 
 export function NavBar() {
   return (
-    <nav className="flex flex-col border-r-2">
+    <nav className="flex flex-col border-r-2 bg-slate-100">
       <header className="grid justify-center gap-3 p-6 pb-4 pt-4 text-center">
         <Avatar fallback="JD" size="7" />
         <Text as="span">John Doe</Text>
@@ -11,11 +12,19 @@ export function NavBar() {
       <Separator size="4" />
 
       <section className="grid h-fill grid-rows-[1fr_auto]">
-        <ul className="p-6">
-          <li>Event Types</li>
-          <li>Scheduled Events</li>
-          <li>Availability</li>
-          <li>Settings</li>
+        <ul className="flex flex-col gap-4 p-6 text-center">
+          <li>
+            <Link href="#">Event Types</Link>
+          </li>
+          <li>
+            <Link href="#">Scheduled Events</Link>
+          </li>
+          <li>
+            <Link href="#">Availability</Link>
+          </li>
+          <li>
+            <Link href="#">Settings</Link>
+          </li>
         </ul>
 
         <div className="p-6 pb-8">Log off</div>
