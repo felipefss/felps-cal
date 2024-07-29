9767955ca4fcf926a33a80fa12b64ff742a935a3
Felipe Santos <felipefss@gmail.com>
2024-07-29 14:55:10 +0100

    feat: redirect main to /event-types

--- src/app/@main/page.tsx
+++ src/app/@main/page.tsx
@@ -1,12 +1,7 @@
 import { redirect } from 'next/navigation';
 
 export default function Home() {
-  // redirect('/event-types');
+  redirect('/event-types');
 
-  return (
-    <main className="">
-      <h1>Home</h1>
-      <p>Welcome to the home page</p>
-    </main>
-  );
+  return null;
 }
