a992f560b26b5ea135e48527dc0f30cb0b40ee7b
Felipe Santos <felipefss@gmail.com>
2024-08-18 15:21:17 +0100

    fix: make attendee email address in a popup

--- src/app/@main/scheduled-events/_components/event.tsx
+++ src/app/@main/scheduled-events/_components/event.tsx
@@ -1,4 +1,4 @@
-import { Button, Popover, Table } from '@radix-ui/themes';
+import { HoverCard, Strong, Table } from '@radix-ui/themes';
 import { QuestionsPopover } from './questions-popover';
 import { ActionsDropdown } from './actions-dropdown';
 
@@ -8,28 +8,38 @@ export function Event() {
       <Table.Cell justify="start" className="align-middle">
         10:00 - 10:30
       </Table.Cell>
+
       <Table.Cell justify="start" className="align-middle">
-        <p>
-          <strong>John Doe</strong>
-        </p>
-        <sub>jonh.doe@example.com</sub>
+        <HoverCard.Root>
+          <HoverCard.Trigger>
+            <p>
+              <Strong>John Doe</Strong>
+            </p>
+          </HoverCard.Trigger>
+          <HoverCard.Content alignOffset={-55}>
+            jonh.doe@example.com
+          </HoverCard.Content>
+        </HoverCard.Root>
       </Table.Cell>
+
       <Table.Cell justify="start" className="align-middle">
         Event Name
       </Table.Cell>
+
       <Table.Cell justify="start" className="align-middle">
         <QuestionsPopover />
       </Table.Cell>
+
       <Table.Cell justify="start" className="align-middle">
         <a href="#" className="text-blue-600">
+          {/* Don't display in Past events */}
           Meeting Link
         </a>
       </Table.Cell>
+
       <Table.Cell justify="start" className="align-middle">
         <ActionsDropdown />
       </Table.Cell>
     </Table.Row>
   );
 }
-
-// Time | Attendee (hover for email address) | Questions | Link | dropdown options (reschedule, cancel)
